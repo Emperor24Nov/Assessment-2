3fbb2f7c57a5ca39711e2c601d798f59f2a3c646
RiffqiDavaSundara <rdavaa@student.telkomuniversity.ac.id>
2024-05-09 10:30:40 +0700

    Menangani keadaan jika list kosong

--- app/src/main/java/org/d3if3143/mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3143/mobpro1/ui/screen/MainScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 import androidx.compose.ui.res.stringResource
@@ -53,12 +54,24 @@ fun ScreenContent(modifier: Modifier) {
     val viewModel: MainViewModel = viewModel()
     val data = viewModel.data
 
-    LazyColumn(
-        modifier = modifier.fillMaxSize()
-    ) {
-        items(data) {
-            ListItem(music = it)
-            Divider()
+    if (data.isEmpty()) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = stringResource(id = R.string.list_kosong))
+        }
+    } else {
+        LazyColumn(
+            modifier = modifier.fillMaxSize()
+        ) {
+            items(data) {
+                ListItem(music = it)
+                Divider()
+            }
         }
     }
 }
