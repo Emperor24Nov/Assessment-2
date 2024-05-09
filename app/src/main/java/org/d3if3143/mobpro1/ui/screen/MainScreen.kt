7b7ff1ae62e22d8e6eb48b4f09b73d4881693031
RiffqiDavaSundara <rdavaa@student.telkomuniversity.ac.id>
2024-05-09 10:27:15 +0700

    Membuat tampilan list

--- app/src/main/java/org/d3if3143/mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3143/mobpro1/ui/screen/MainScreen.kt
@@ -1,9 +1,14 @@
 package org.d3if3143.mobpro1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -14,28 +19,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.d3if3143.mobpro1.R
+import org.d3if3143.mobpro1.model.Music
 import org.d3if3143.mobpro1.ui.theme.Mobpro1Theme
-
-@Composable
-fun ScreenContent(modifier: Modifier) {
-    Column (
-        modifier = modifier.fillMaxSize().padding(16.dp)
-    ){
-
-    }
-}
-
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
-@Composable
-fun ScreenPreview() {
-    Mobpro1Theme {
-        Mainscreen()
-    }
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -54,5 +45,61 @@ fun Mainscreen() {
         }
     ) { padding ->
         ScreenContent(Modifier.padding(padding))
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier) {
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
+
+    LazyColumn(
+        modifier = modifier.fillMaxSize()
+    ) {
+        items(data) {
+            ListItem(music = it)
+            Divider()
+        }
+    }
+}
+
+@Composable
+fun ListItem(music: Music) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = music.judul,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = music.nama,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = music.tahun,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = music.genre,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun ScreenPreview() {
+    Mobpro1Theme {
+        Mainscreen()
     }
 }
